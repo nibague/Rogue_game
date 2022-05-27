c934c7945b9f52d776c871b4176f8c7579517bf3
nibague <nikolasibagues@gmail.com>
2022-05-27 14:01:41 -0500

    creating react app and installing tailwindcss for styles

---
+++ rogue-game/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Name from './components/Name';
+
+function App() {
+  return (
+    <div className='bg-gray-500 text-white m-4 p-10'>
+      <Name/>
+    </div>
+  );
+}
+
+
+export default App;
---
+++ rogue-game/src/components/Name.jsx
@@ -0,0 +1,10 @@
+function Name(){
+    return (<div> 
+      <div className='bg-slate-400 m-5 rounded-lg text-white p-7'>
+        This is a box
+      </div>
+    </div>);
+  }
+
+
+export default Name
