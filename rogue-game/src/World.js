577e45cba06b5d48f043f7af3285dd4f41ac45e7
nibague <nikolasibagues@gmail.com>
2022-06-06 22:33:22 -0500

    creating a map object and using cellular method to create the map

--- rogue-game/src/World.js
+++ rogue-game/src/World.js
@@ -1,3 +1,6 @@
+import { Map } from 'rot-js';
+
+
 class World {
     constructor(width, height, tilesize){
         this.width = width;
@@ -7,16 +10,32 @@ class World {
         for ( let x = 0; x<this.width;x++){
             this.worldmap[x] = new Array(this.height);
         }
-        this.createRandomMap();
+        this.createCellularMap();
     }
 
-    createRandomMap(){
+    createCellularMap() {
+
+        var map = new Map.Cellular(this.width, this.height, {connected: true})
+        map.randomize(0.5);
+        var userCallback = (x, y, value) => {
+            if(x === 0 || y === 0 || x === this.width -1 || y === this.height -1){
+                this.worldmap[x][y] = 1;
+                return;
+            }
+
+            this.worldmap[x][y] = value === 0 ? 1 : 0;
+        };
+        map.create(userCallback)
+        map.connect(userCallback, 1);
+    }
+
+    /* createRandomMap(){
         for ( let x = 0; x<this.width;x++){
             for ( let y = 0; y<this.height;y++){
                 this.worldmap[x][y] = Math.round(Math.random());
             }
         }
-    }
+    } */
 
     draw(context){
         for ( let x = 0; x<this.width;x++){
